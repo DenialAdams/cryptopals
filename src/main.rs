896fde8f4276889fcdca8781c46af1e00d2b364c
Richard McCormack <brick@brick.codes>
2017-06-26 18:24:03 -0400

    cool equals sign thing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use std::io;
 
-const BASE64: [char; 64] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'];
+const BASE64:           [char; 64] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'];
+const BASE64_REMAINDER: [char; 64] = ['=', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'];
+
 
 fn main() {
   let mut input = String::new();
@@ -9,31 +11,38 @@ fn main() {
 }
 
 fn hextobase64(input: String) -> Result<String, &'static str> {
-  let mut sum: u16 = 0;
+  if input.len() % 2 != 0 {
+    return Err("odd size hex string");
+  }
+  let mut sum: u32 = 0;
   let mut i = 0;
   // This is reserving too much, TODO
   let mut new_str = String::with_capacity(input.len());
   for c in input.chars() {
-    let val = hex_to_u16(c)?;
+    let val = hex_to_u32(c)?;
     sum |= val;
     sum <<= 4;
     i += 1;
-    if i == 3 {
-      new_str.push(BASE64[((sum & 0b1111_1100_0000_0000) >> 10) as usize]);
-      new_str.push(BASE64[((sum & 0b0000_0011_1111_0000) >> 4) as usize]);
+    if i == 6 {
+      new_str.push(BASE64[((sum & 0x0fc0_0000) >> 22) as usize]);
+      new_str.push(BASE64[((sum & 0x003f_0000) >> 16) as usize]);
+      new_str.push(BASE64[((sum & 0x0000_fc00) >> 10) as usize]);
+      new_str.push(BASE64[((sum & 0x0000_03f0) >> 4) as usize]);
       i = 0;
       sum = 0;
     }
   }
   if i != 0 {
-    sum <<= 4 * (3 - i);
-    new_str.push(BASE64[((sum & 0b1111_1100_0000_0000) >> 10) as usize]);
-    new_str.push(BASE64[((sum & 0b0000_0011_1111_0000) >> 4) as usize]);
+    sum <<= 4 * (6 - i);
+    new_str.push(BASE64[((sum & 0x0fc0_0000) >> 22) as usize]);
+    new_str.push(BASE64[((sum & 0x003f_0000) >> 16) as usize]);
+    new_str.push(BASE64_REMAINDER[((sum & 0x0000_fc00) >> 10) as usize]);
+    new_str.push(BASE64_REMAINDER[((sum & 0x0000_03f0) >> 4) as usize]);
   }
   Ok(new_str)
 }
 
-fn hex_to_u16(input: char) -> Result<u16, &'static str> {
+fn hex_to_u32(input: char) -> Result<u32, &'static str> {
   match input {
     '0' => {
       Ok(0)
@@ -94,9 +103,16 @@ mod tests {
     use hextobase64;
 
     #[test]
-    fn hextobase64_test() {
+    fn hextobase64_exact_test() {
       let input = String::from("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d");
       let output = String::from("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t");
+      assert_eq!(hextobase64(input).unwrap(), output);
+    }
+
+    #[test]
+    fn hextobase64_remainder_test() {
+      let input = String::from("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6ddd");
+      let output = String::from("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t3Q==");
       assert_eq!(hextobase64(input).unwrap(), output);
     }
 }
