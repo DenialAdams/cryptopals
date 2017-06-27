8c3da3f6cbfd9f08d281eb6f5a90cd2307b5e266
Richard McCormack <brick@brick.codes>
2017-06-27 15:52:59 -0400

    repeating key XOR (challenge 5)

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use std::fs::File;
 use std::io::{self, BufReader, BufRead};
 
 const BASE64: [char; 64] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/'];
+const HEX:    [&'static str; 256] = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "0a", "0b", "0c", "0d", "0e", "0f", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "1a", "1b", "1c", "1d", "1e", "1f", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "2a", "2b", "2c", "2d", "2e", "2f", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "3a", "3b", "3c", "3d", "3e", "3f", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "4a", "4b", "4c", "4d", "4e", "4f", "50", "51", "52", "53", "54", "55", "56", "57", "58", "59", "5a", "5b", "5c", "5d", "5e", "5f", "60", "61", "62", "63", "64", "65", "66", "67", "68", "69", "6a", "6b", "6c", "6d", "6e", "6f", "70", "71", "72", "73", "74", "75", "76", "77", "78", "79", "7a", "7b", "7c", "7d", "7e", "7f", "80", "81", "82", "83", "84", "85", "86", "87", "88", "89", "8a", "8b", "8c", "8d", "8e", "8f", "90", "91", "92", "93", "94", "95", "96", "97", "98", "99", "9a", "9b", "9c", "9d", "9e", "9f", "a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9", "aa", "ab", "ac", "ad", "ae", "af", "b0", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9", "ba", "bb", "bc", "bd", "be", "bf", "c0", "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9", "ca", "cb", "cc", "cd", "ce", "cf", "d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9", "da", "db", "dc", "dd", "de", "df", "e0", "e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8", "e9", "ea", "eb", "ec", "ed", "ee", "ef", "f0", "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9", "fa", "fb", "fc", "fd", "fe", "ff"];
 
 fn main() {
-  let res = best_decrypt_single_byte_xor_file().unwrap();
-  println!("{}", res);
+  println!("Hello, world!");
 }
 
 fn hextobase64(input: &str) -> Result<String, &'static str> {
@@ -45,15 +45,14 @@ fn hextobase64(input: &str) -> Result<String, &'static str> {
   Ok(new_str)
 }
 
-fn best_decrypt_single_byte_xor_file() -> Result<String, &'static str> {
-  let f = File::open("4.txt").unwrap();
+fn best_decrypt_single_byte_xor_file(file_name: &str) -> Result<String, &'static str> {
+  let f = File::open(file_name).unwrap();
   let f = BufReader::new(f);
 
   let mut best_str = String::from("test");
   let mut best_score = 0.0;
   for line in f.lines() {
     let (new_str, new_score) = best_decrypt_single_byte_xor(&line.unwrap()).unwrap();
-    println!("{}", new_score);
     if new_score > best_score || best_score == 0.0 {
       best_score = new_score;
       best_str = new_str;
@@ -61,6 +60,17 @@ fn best_decrypt_single_byte_xor_file() -> Result<String, &'static str> {
   }
 
   Ok(best_str)
+}
+
+fn repeating_key_xor(foo: &str, key: &str) -> Result<String, &'static str> {
+  let mut new_str = String::with_capacity(foo.len());
+  for it in foo.bytes().zip(key.bytes().cycle()) {
+    let (c, x) = it;
+    let xor_val = c ^ x;
+    new_str.push_str(HEX[xor_val as usize]);
+    println!("{} {} {:02x}", c as char, x as char, xor_val);
+  }
+  Ok(new_str)
 }
 
 fn fixed_xor(foo: &str, bar: &str) -> Result<String, &'static str> {
@@ -318,5 +328,21 @@ mod tests {
       let input1 = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
       let output = "Cooking MC's like a pound of bacon";
       assert_eq!(best_decrypt_single_byte_xor(input1).unwrap().0, output);
+    }
+
+    #[test]
+    fn best_decrypt_single_byte_xor_file_test() {
+      use best_decrypt_single_byte_xor_file;
+      let output = "Now that the party is jumping\n";
+      assert_eq!(best_decrypt_single_byte_xor_file("4.txt").unwrap(), output);
+    }
+
+    #[test]
+    fn repeating_key_xor_test() {
+      use repeating_key_xor;
+      let input = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+      let key = "ICE";
+      let output = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
+      assert_eq!(repeating_key_xor(input, key).unwrap(), output);
     }
 }
